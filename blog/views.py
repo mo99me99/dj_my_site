5981be5fda00ab3c0c382b373e037f7b61534ca7
mo99me99 <m.m.hosseini1099@gmail.com>
2024-07-13 17:25:11 +0330

    add urls and views for blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def starting_page(request):
+    return HttpResponse('Hello this is index page')
+
+
+def posts(request):
+    pass
+
+
+def post_detail(request, slug):
+    pass
